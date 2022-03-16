486aa26b6041785b3554a7b26462e3ddaefc57e9
compuwill <wpvorngsam@hotmail.com>
2022-03-15 20:02:42 -0500

    added test for Employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,10 @@
 const Employee = require('../lib/Employee');
 
-test
+test('create an Employee object', () => {
+    const employee = new Employee('Todd', 0, 'todd@mail.com');
+
+    expect(employee.name).toBe('Todd');
+    expect(employee.id).toBe(0);
+    expect(employee.email).toBe('todd@mail.com');
+
+});
